cb5bdba6919e39b691d6881d9d36d37f69513e3c
Lachlan Grose <lachlan.grose@monash.edu>
2024-02-09 09:16:18 +1100

    fix: complete example

--- examples/create_mesh.py
+++ examples/create_mesh.py
@@ -5,7 +5,8 @@ from loopstructuralmeshing import MeshFactory
 data, bb = load_claudius()
 
 model = GeologicalModel(bb[0, :], bb[1, :])
+model.data = data
+model.create_and_add_foliation("strati", interpolator="PLI", nelements=1e4)
 
-mesh = MeshFactory.create_mesh(model.bounding_box, order=1, n_elements=1e4)
 
-mesh.
+mesh = MeshFactory.create_mesh(model.bounding_box, order=2, n_elements=1e4)
